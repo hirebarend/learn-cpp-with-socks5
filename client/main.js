446f940456998e4ee63db38adcd98c6f1489fdd2
Barend Erasmus <hirebarend@gmail.com>
2026-05-10 13:02:37 +0200

    add nodejs client

---
+++ client/main.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { SocksProxyAgent } from "socks-proxy-agent";
+
+await new Promise((resolve) => setTimeout(resolve, 3000));
+
+const uri = "socks5://username:password@127.0.0.1:8080";
+
+const socksProxyAgent = new SocksProxyAgent(uri);
+
+const res = await axios.get("https://api.ipify.org?format=json", {
+  httpAgent: socksProxyAgent,
+  httpsAgent: socksProxyAgent,
+  proxy: false,
+  timeout: 15000,
+});
+
+console.log(res.data);
